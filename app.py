b1d0c54834e6b62365531cf65c49e53e9af26294
HARSH SINGH <131766995+dsharshthakur@users.noreply.github.com>
2023-12-12 15:43:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -41,7 +41,7 @@ progress_text = "Operation in progress. Please wait."
 my_bar = st.progress(0, text=progress_text)
 
 for percent_complete in range(100):
-    time.sleep(0.02)
+    time.sleep(0.01)
     my_bar.progress(percent_complete + 1, text=progress_text)
 time.sleep(1)
 my_bar.empty()
